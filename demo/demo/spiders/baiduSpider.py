cb367029ac45d80c2d13de3fea7415f3b8116ea4
HongqiaoChen <1214120451@qq.com>
2020-08-09 19:03:32 +0800

    Initial commit

---
+++ demo/demo/spiders/baiduSpider.py
@@ -0,0 +1,14 @@
+import scrapy
+
+
+class BaiduspiderSpider(scrapy.Spider):
+    name = 'baiduSpider'
+    allowed_domains = ['baidu.com']
+    start_urls = ['http://www.baidu.com/']
+
+    def parse(self, response):
+
+        print(response.text)
+
+        # print(response.headers)
+        # print(response.status)
